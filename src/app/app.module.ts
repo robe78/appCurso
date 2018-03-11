f467ba2f57aba53da799ddd0e52598e1bbc11bc4
Roberto Costa <rob.antunez@gmail.com>
2018-03-11 21:54:26 +0000

    Primera parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FechaactualComponent } from './fechaactual/fechaactual.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FechaactualComponent,
+    CopyrightComponent,
+    ViewmodeloComponent
   ],
   imports: [
     BrowserModule
